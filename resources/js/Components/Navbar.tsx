4a9029e8fc4b100112941372df5bfb1fcdc87658
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 17:22:29 +0100

    refactor: lifted the state up

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -3,28 +3,15 @@ import NavbarLinks from "./NavbarLinks";
 import NavbarIcons from "./NavbarIcons";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
-import Menu_Searchbar_Context from "@/Contexts/Menu_Searchbar_Context";
 
 const Navbar = () => {
-    const [isMenuVisible, setIsMenuVisible] = useState(false);
-    const [isSearchbarVisible, setIsSearchbarVisible] = useState(false);
-
     return (
-        <Menu_Searchbar_Context.Provider
-            value={{
-                isMenuVisible: isMenuVisible,
-                setIsMenuVisible: setIsMenuVisible,
-                isSearchbarVisible: isSearchbarVisible,
-                setIsSearchbarVisible: setIsSearchbarVisible,
-            }}
-        >
-            <nav className=" bg-black flex px-mobile_side_padding items-center justify-between border-b border-border_color  border-solid lg:justify-normal">
-                <Logo />
-                <NavbarIcons />
-                <NavbarLinks />
-                <SearchBar />
-            </nav>
-        </Menu_Searchbar_Context.Provider>
+        <nav className=" bg-black flex px-mobile_side_padding items-center justify-between border-b border-border_color  border-solid lg:justify-normal">
+            <Logo />
+            <NavbarIcons />
+            <NavbarLinks />
+            <SearchBar />
+        </nav>
     );
 };
 
--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -1,16 +1,26 @@
 import Navbar from "@/Components/Navbar";
-import React from "react";
+import React, { useState } from "react";
 import "../Helpers/CSS/borders.css";
 import "./CSS/AppLayout.css";
+import Menu_Searchbar_Context from "@/Contexts/Menu_Searchbar_Context";
 
 const AppLayout = ({ user, children, className = "" }) => {
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [isSearchbarVisible, setIsSearchbarVisible] = useState(false);
     return (
-        <>
+        <Menu_Searchbar_Context.Provider
+            value={{
+                isMenuVisible: isMenuVisible,
+                setIsMenuVisible: setIsMenuVisible,
+                isSearchbarVisible: isSearchbarVisible,
+                setIsSearchbarVisible: setIsSearchbarVisible,
+            }}
+        >
             <Navbar />
             <section className={`px-mobile_side_padding  ${className}`}>
                 {children}
             </section>
-        </>
+        </Menu_Searchbar_Context.Provider>
     );
 };
 
