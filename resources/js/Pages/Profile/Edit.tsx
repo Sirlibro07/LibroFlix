91e3ccf19c1970008b6d7328afa0ffdf3557e5b8
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 14:53:39 +0100

    feat: added options to profile page

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -4,6 +4,7 @@ import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
 import { Head } from "@inertiajs/react";
 import React from "react";
+import OptionsList from "@/Components/OptionsList";
 
 export default function Edit({ auth, mustVerifyEmail, status }) {
     return (
@@ -12,6 +13,16 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
             <header className="font-display text-mobile_h1 text-contrast">
                 Account
             </header>
+
+            <OptionsList
+                options={[
+                    { name: "Profile" },
+                    { name: "Update Password" },
+                    { name: "Other" },
+                ]}
+                ul_className="mt-8"
+            />
+
             <UpdateProfileInformationForm
                 mustVerifyEmail={mustVerifyEmail}
                 status={status}
