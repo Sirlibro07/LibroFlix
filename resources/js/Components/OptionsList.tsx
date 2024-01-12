50f90ea8c085fc59cc6825bd5c9ca9ef12e25388
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 17:01:57 +0100

    feat: added onClick feature to show only the current option (form)

--- resources/js/Components/OptionsList.tsx
+++ resources/js/Components/OptionsList.tsx
@@ -5,13 +5,23 @@ type option = { name: string };
 interface OptionsListProps {
     options: option[];
     ul_className?: string;
+    setCurrentOption: any;
 }
 
-const OptionsList = ({ options, ul_className = "" }: OptionsListProps) => {
+const OptionsList = ({
+    options,
+    ul_className = "",
+    setCurrentOption,
+}: OptionsListProps) => {
     return (
         <ul className={`flex gap-6 ${ul_className}`}>
             {options.map((option: option, index: React.Key) => (
-                <li key={index}>
+                <li
+                    key={index}
+                    onClick={() => {
+                        setCurrentOption(option.name);
+                    }}
+                >
                     <button className="custom_border relative text-white font-normal">
                         {option.name}
                     </button>
--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -3,10 +3,23 @@ import DeleteUserForm from "./Partials/DeleteUserForm";
 import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformation";
 import { Head } from "@inertiajs/react";
-import React from "react";
+import React, { useState } from "react";
 import OptionsList from "@/Components/OptionsList";
 
 export default function Edit({ auth, mustVerifyEmail, status }) {
+    const [currentOption, setCurrentOption] = useState("Other");
+
+    const renderCurrentOption = () => {
+        switch (currentOption) {
+            case "Profile":
+                return <UpdateProfileInformationForm />;
+            case "Password":
+                return <UpdatePasswordForm />;
+            case "Other":
+                return <DeleteUserForm />;
+        }
+    };
+
     return (
         <App user={auth.user} className="pt-8">
             <Head title="Profile" />
@@ -20,14 +33,11 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
                     { name: "Password" },
                     { name: "Other" },
                 ]}
+                setCurrentOption={setCurrentOption}
                 ul_className="mt-8 mb-16"
             />
 
-            <UpdateProfileInformationForm />
-
-            {/* <UpdatePasswordForm />
-
-            <DeleteUserForm /> */}
+            {renderCurrentOption()}
         </App>
     );
 }
