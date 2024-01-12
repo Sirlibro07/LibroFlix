bac30308857c081f3d8539464ff50aa7afe49015
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 21:07:17 +0100

    style: updated lg styles of the NavbarAccountLinks

--- resources/js/Components/NavbarAccountLinks.tsx
+++ resources/js/Components/NavbarAccountLinks.tsx
@@ -4,7 +4,7 @@ import PrimaryButton from "./PrimaryButton";
 
 const NavbarAccountLinks = () => {
     return (
-        <div className="text-base px-3 sm:flex sm:gap-x-3 lg:gap-x-6">
+        <div className="text-base px-3 sm:flex sm:gap-x-3 lg:absolute lg:top-0 lg:right-0 lg:flex lg:items-center lg:h-full lg:gap-x-6">
             <SecondaryButton className="lg:w-[91px]">Log In</SecondaryButton>
             <PrimaryButton
                 custom_margin_top="mt-2"
