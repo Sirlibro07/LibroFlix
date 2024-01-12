053bfd4d18d6c175d26a9f0f609f22ece36c7cc1
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 20:00:55 +0100

    style: added dinamic style based on user status

--- TODO.md
+++ TODO.md
@@ -6,11 +6,6 @@ Project Description
 
 ### Todo
 
-- [ ] REFACTOR: icons navbar handleonClick make it into an helper function, apply the useContext hook in the navbar props  
-- [ ] FrontEnd: implement half border on Navbar Links  
-- [ ] Backend: implement forgot password email try catch  
-- [ ] Frontend-Auth: make real time validations with icons  
-- [ ] Frontend-Auth: make animations that when i click on link the div goes up/down useEffect maybe  
 
 ### In Progress
 
--- resources/js/Components/NavbarLinks.tsx
+++ resources/js/Components/NavbarLinks.tsx
@@ -24,7 +24,9 @@ const NavbarLinks = () => {
         <ul
             className={`gap-y-12  rounded-bl-xl text-typescale_21_px flex flex-col border border-t-0 z-10 border-border_color  border-solid text-contrast absolute top-[5.688rem] w-[90%]   right-0 transition-all duration-200 ${
                 isMenuVisible ? "flex" : "hidden"
-            } h-fit pt-12 pb-4 bg-body lg:flex lg:text-base lg:flex-row lg:gap-y-0 lg:gap-x-6 lg:w-fit lg:h-fit lg:py-0 lg:static lg:mx-12 lg:border-0  lg:font-normal  `}
+            } h-fit pt-12 ${
+                isLoggedIn ? "pb-12" : "pb-4"
+            } bg-body lg:flex lg:text-base lg:flex-row lg:gap-y-0 lg:gap-x-6 lg:w-fit lg:h-fit lg:py-0 lg:static lg:mx-12 lg:border-0  lg:font-normal `}
         >
             {routes.map((link, index) => (
                 <NavbarLink link={link} key={index} />
