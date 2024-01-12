9bad836bb284e91d85fc3157a54f0bc32fa44ce8
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 17:14:42 +0100

    style: added layout css

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -1,14 +1,13 @@
 import Navbar from "@/Components/Navbar";
 import React from "react";
 import "../Helpers/CSS/borders.css";
+import "./CSS/AppLayout.css";
 
 const AppLayout = ({ user, children, className = "" }) => {
     return (
         <>
             <Navbar />
-            <section
-                className={`yo bg-black  px-mobile_side_padding  ${className}`}
-            >
+            <section className={`yo   px-mobile_side_padding  ${className}`}>
                 {children}
             </section>
         </>
