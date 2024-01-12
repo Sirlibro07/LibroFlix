d5a179dc5b999590ce065786ca8d2c5c1cd72673
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-12 15:18:44 +0100

    feat: added new Form similar to AuthForm but for Profile

---
+++ resources/js/Components/ProfileForm.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PrimaryButton from "./PrimaryButton";
+
+const ProfileForm = ({
+    children,
+    method_function,
+    route_name,
+    processing,
+    recentlySuccessful,
+}) => {
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        method_function(route(route_name));
+    };
+    return (
+        <form onSubmit={submit}>
+            {recentlySuccessful && (
+                <small className="text-white block">Updated</small>
+            )}
+            {children}
+            <PrimaryButton disabled={processing}>Update</PrimaryButton>
+        </form>
+    );
+};
+
+export default ProfileForm;
