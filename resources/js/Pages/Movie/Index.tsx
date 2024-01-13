2d8c2af87653409969991bc2bc25118a313b5938
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-13 15:40:12 +0100

    style: added bg-image

--- resources/js/Pages/Movie/Index.tsx
+++ resources/js/Pages/Movie/Index.tsx
@@ -1,10 +1,14 @@
+import getImg from "@/Helpers/getImg";
 import AppLayout from "@/Layouts/AppLayout";
 import React from "react";
 
 const Index = ({ auth, isLoggedIn }) => {
+    const img = (version: string) => {
+        return getImg("movies", version);
+    };
     return (
         <AppLayout user={auth.user} isLoggedIn={isLoggedIn}>
-            <></>
+            <img alt="background-image" src={img("desktop")} decoding="async" />
         </AppLayout>
     );
 };
