d1a5847020c32f798c0897442d90b502ba1d6672
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-13 15:14:13 +0100

    fix: fixed css issue of buttons width

--- resources/js/Components/NavbarAccountLinks.tsx
+++ resources/js/Components/NavbarAccountLinks.tsx
@@ -5,12 +5,12 @@ import PrimaryButton from "./PrimaryButton";
 const NavbarAccountLinks = () => {
     return (
         <div className="text-base  px-3 sm:flex sm:gap-x-3 lg:absolute lg:top-0 lg:right-0 lg:flex lg:items-center lg:h-full lg:mr-4">
-            <a href={route("login")}>
+            <a href={route("login")} className="block w-full">
                 <SecondaryButton className="lg:w-[91px]">
                     Log In
                 </SecondaryButton>
             </a>
-            <a href={route("register")}>
+            <a href={route("register")} className="block w-full">
                 <PrimaryButton
                     custom_margin_top="mt-2"
                     className="sm:mt-0  lg:w-[105px] lg:h-normal_btn"
