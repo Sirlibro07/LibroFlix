da5bc64e1e51dafdf140861781d30ec849db4420
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-13 15:20:02 +0100

    fix: added AuthCheck to movies page

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -13,7 +14,7 @@ class MovieController extends Controller
      */
     public function index(): Response
     {
-        return Inertia::render('Movie/Index', []);
+        return Inertia::render('Movie/Index', ['isLoggedIn' => Auth::check(),]);
     }
 
     /**
