60f84da0a84f4fa5b895cad3d81eb8372c24ef76
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-14 16:45:17 +0100

    refactor: removed useless style

--- resources/js/Components/FirstMovieDescription.tsx
+++ resources/js/Components/FirstMovieDescription.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FirstMovieDescription = () => {
     return (
         <div className="px-[44px] text-center  z-1 ">
-            <h1 className="text-white font-bold text-[33px]">
+            <h1 className="font-bold text-[33px]">
                 Naruto Shippuden: The Movie
             </h1>
         </div>
