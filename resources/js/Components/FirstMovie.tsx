aff1e4cd85ff959ed9f19de63dd531327ec8db5f
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-14 18:42:09 +0100

    refactor: added indentation

--- resources/js/Components/FirstMovie.tsx
+++ resources/js/Components/FirstMovie.tsx
@@ -13,10 +13,10 @@ const FirstMovie = () => {
                 <MovieInfo date={"2007"} />
                 <MovieDescription
                     description="So I started to walk into the water. I won't lie to you boys, I was
-            terrified. But I pressed on, and as I made my way past the breakers
-            a strange calm came over me. I don't know if it was divine
-            intervention or the kinship of all living things but I tell you
-            Jerry at that moment, I was a marine biologist."
+                                terrified. But I pressed on, and as I made my way past the breakers
+                                a strange calm came over me. I don't know if it was divine
+                                intervention or the kinship of all living things but I tell you
+                                Jerry at that moment, I was a marine biologist."
                 />
 
                 <GeneralLink
