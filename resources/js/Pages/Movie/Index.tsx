8f3d53632058bea91f1ee96c23120df0565633b1
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-14 16:23:53 +0100

    style: added FirstMovieDesc component to Movie page

--- resources/js/Pages/Movie/Index.tsx
+++ resources/js/Pages/Movie/Index.tsx
@@ -1,4 +1,5 @@
 import BackgroundImage from "@/Components/BackgroundImage";
+import FirstMovieDescription from "@/Components/FirstMovieDescription";
 import getBackgroundImg from "@/Helpers/getBackgroundImg";
 import AppLayout from "@/Layouts/AppLayout";
 import React from "react";
@@ -10,9 +11,12 @@ const Index = ({ auth, isLoggedIn }) => {
     return (
         <AppLayout user={auth.user} isLoggedIn={isLoggedIn}>
             <BackgroundImage
-                picture_classes="w-full h-[40rem] sm:h-[48.75rem]"
+                picture_classes="w-full h-full z-[0] sm:h-[48.75rem]"
                 img_function={img}
             />
+            <div className="absolute z-1">
+                <FirstMovieDescription />
+            </div>
         </AppLayout>
     );
 };
