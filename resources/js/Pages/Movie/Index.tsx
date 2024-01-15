ea4e5e8ea1c0727e058871afbe0a457ea94ae7b5
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-15 14:32:34 +0100

    Revert "style: removed useless absolute positioning on FirstMovieParentDiv"
    This reverts commit 4bf06d3cc8625efbb028abbcc1c835e363d6f278.

--- resources/js/Pages/Movie/Index.tsx
+++ resources/js/Pages/Movie/Index.tsx
@@ -2,21 +2,20 @@ import BackgroundImage from "@/Components/BackgroundImage";
 import FirstMovie from "@/Components/FirstMovie";
 import getBackgroundImg from "@/Helpers/getBackgroundImg";
 import AppLayout from "@/Layouts/AppLayout";
-import React, { useContext } from "react";
+import React from "react";
 
 const Index = ({ auth, isLoggedIn }) => {
     const img = (version: string) => {
         return getBackgroundImg("movies", version);
     };
-
     return (
         <AppLayout user={auth.user} isLoggedIn={isLoggedIn}>
             <BackgroundImage
-                picture_classes="w-full top-[5.625rem] h-[627px] z-[-1] sm:h-[48.75rem]"
+                picture_classes="w-full top-[5.625rem] h-[627px] z-[0] sm:h-[48.75rem]"
                 img_function={img}
             />
 
-            <div className="w-full text-white">
+            <div className="absolute left-0 w-full text-white mt-[180px]">
                 <FirstMovie />
             </div>
         </AppLayout>
