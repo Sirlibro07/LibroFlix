cd77059a8a033b39347d6b7ec7d051487d9bad9a
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-16 00:25:53 +0100

    style: added MovieInfo component className prop

--- resources/js/Components/MovieInfo.tsx
+++ resources/js/Components/MovieInfo.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 interface MovieInfoProps {
     date: string;
+    className?: string;
 }
 
-const MovieInfo = ({ date }: MovieInfoProps) => {
+const MovieInfo = ({ date, className }: MovieInfoProps) => {
     return (
-        <div className="flex items-center justify-center gap-x-2 text-typescale_19_px mt-4 mb-2">
+        <div
+            className={`flex items-center justify-center gap-x-2 text-typescale_19_px mt-4 mb-2 ${className}`}
+        >
             <p className="font-medium ">Studio Pierrot</p>
             <div className="w-[7px] h-[7px] bg-light_color rounded-full"></div>
             <p className="text-light_color">{date}</p>
