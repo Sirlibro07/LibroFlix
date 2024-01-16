78bb7739514044cac1116e784ae4156aefedde23
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-16 18:59:31 +0100

    style: updated MovieInfo font-weight

--- resources/js/Components/MovieInfo.tsx
+++ resources/js/Components/MovieInfo.tsx
@@ -10,7 +10,7 @@ const MovieInfo = ({ date, className }: MovieInfoProps) => {
         <div
             className={`flex items-center justify-center gap-x-2 text-typescale_19_px mt-4 mb-2 ${className}`}
         >
-            <p className="font-medium ">Studio Pierrot</p>
+            <p>Studio Pierrot</p>
             <div className="w-[7px] h-[7px] bg-light_color rounded-full"></div>
             <p className="text-light_color">{date}</p>
         </div>
