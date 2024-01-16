03ec4accde0da0c33376257f7a9b8e6fc8149aa1
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-16 21:25:11 +0100

    feat: added closing of navbar items on click

--- resources/js/Components/AppLayoutChildren.tsx
+++ resources/js/Components/AppLayoutChildren.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useContext } from "react";
 import Navbar from "./Navbar";
+import Menu_Searchbar_Context from "@/Contexts/Menu_Searchbar_Context";
 
 interface AppLayoutChildrenProps {
     className: string;
@@ -7,10 +8,26 @@ interface AppLayoutChildrenProps {
 }
 
 const AppLayoutChildren = ({ className, children }: AppLayoutChildrenProps) => {
+    const setIsMenuVisible = useContext(
+        Menu_Searchbar_Context
+    ).setIsMenuVisible;
+
+    const setIsSearchbarVisible = useContext(
+        Menu_Searchbar_Context
+    ).setIsSearchbarVisible;
+
+    const onClickHandler = () => {
+        setIsMenuVisible(false);
+        setIsSearchbarVisible(false);
+    };
+
     return (
         <>
             <Navbar />
-            <section className={`body-padding ${className}`}>
+            <section
+                className={`body-padding ${className}`}
+                onClick={onClickHandler}
+            >
                 {children}
             </section>
         </>
