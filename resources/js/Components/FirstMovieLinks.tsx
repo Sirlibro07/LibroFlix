ccc29312b4657a619a9464773b26a153cd08a20a
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-16 01:13:40 +0100

    style: added link with icons to FirstMovieLinks

--- resources/js/Components/FirstMovieLinks.tsx
+++ resources/js/Components/FirstMovieLinks.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import GeneralLink from "./GeneralLink";
+import GeneralLinkWithIcon from "./GeneralLinkWithIcon";
 
 const FirstMovieLinks = () => {
     return (
-        <GeneralLink
-            route_name={"home"}
-            border={false}
-            className="bg-brand font-medium block text-center py-[0.625rem] mt-6 "
-        >
-            Watch Now
-        </GeneralLink>
+        <>
+            <GeneralLink
+                route_name={"home"}
+                border={false}
+                className="bg-brand font-medium block text-center py-[0.625rem] mt-6 md:hidden "
+            >
+                Watch Now
+            </GeneralLink>
+            <div className="w-full   flex gap-x-5">
+                <GeneralLinkWithIcon
+                    route_name={"home"}
+                    className="bg-brand"
+                    icon="play"
+                >
+                    Play
+                </GeneralLinkWithIcon>
+                <GeneralLinkWithIcon
+                    route_name={"home"}
+                    className="bg-secondary"
+                    icon="plus"
+                >
+                    Learn More
+                </GeneralLinkWithIcon>
+            </div>
+        </>
     );
 };
 
