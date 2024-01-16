1d3f01ca2388a3859d9f2e4cc4763e0c1b287815
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-16 19:11:58 +0100

    style: updated MoviesPage Content margin-top

--- resources/js/Components/MoviesIndexContent.tsx
+++ resources/js/Components/MoviesIndexContent.tsx
@@ -24,7 +24,7 @@ const MoviesIndexContent = () => {
                 img_function={img}
             />
             <div
-                className={`absolute left-0 w-full text-white mt-[9rem] ${conditional_style}`}
+                className={`absolute left-0 w-full text-white mt-[8rem] md:mt-[9rem] ${conditional_style}`}
             >
                 <FirstMovie />
             </div>
