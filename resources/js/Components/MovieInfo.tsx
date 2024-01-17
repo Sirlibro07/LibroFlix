a8b22a98091f095d7208bac9b977f035f2727f33
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-17 11:29:55 +0100

    style: updated MovieInfo margin-top

--- resources/js/Components/MovieInfo.tsx
+++ resources/js/Components/MovieInfo.tsx
@@ -8,7 +8,7 @@ interface MovieInfoProps {
 const MovieInfo = ({ date, className }: MovieInfoProps) => {
     return (
         <div
-            className={`flex items-center justify-center gap-x-2 text-typescale_19_px mt-4 mb-2 ${className}`}
+            className={`flex items-center justify-center gap-x-2 text-typescale_19_px  mb-2 ${className}`}
         >
             <p>Studio Pierrot</p>
             <div className="w-[7px] h-[7px] bg-light_color rounded-full"></div>
