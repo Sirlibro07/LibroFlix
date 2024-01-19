509fc99443988a930909f39e7615d74b1b9662de
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-19 22:16:34 +0100

    style: coded Slider functionality and styles

--- resources/js/Components/Slider.tsx
+++ resources/js/Components/Slider.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const Slider = () => {
-    return <></>;
+interface SliderProps {
+    children: ReactNode;
+}
+
+const Slider = ({ children }: SliderProps) => {
+    return (
+        <section className="w-full  grid grid-cols-2 gap-4  md:flex md:overflow-hidden xs:grid-cols-3  ">
+            {children}
+        </section>
+    );
 };
 
 export default Slider;
