9179fc89848d7cd70a47a3148f3ab2c7b6ebaebe
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-19 22:26:48 +0100

    style: created MovieCardPlayButton

---
+++ resources/js/Components/MovieCardPlayButton.tsx
@@ -0,0 +1,16 @@
+import imgStoragePath from "@/Helpers/imgStoragePath";
+import React from "react";
+
+const MovieCardPlayButton = () => {
+    const img = () => {
+        return imgStoragePath("cards/icons/play");
+    };
+
+    return (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[45%] md:w-[20%]">
+            <img src={img()} alt="play-icon" decoding="async" />
+        </div>
+    );
+};
+
+export default MovieCardPlayButton;
