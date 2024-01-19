f2a7949b8daf1323bf0a398e12a5a92e482fc5df
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-19 22:27:05 +0100

    style: created MovieCardContent

---
+++ resources/js/Components/MovieCardContent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MovieCardPlayButton from "./MovieCardPlayButton";
+import MovieCardTitle from "./MovieCardTitle";
+
+const MovieCardContent = () => {
+    return (
+        <div className="opacity-0 transition-all duration-300 group-hover:opacity-100">
+            <MovieCardPlayButton />
+            <MovieCardTitle title={"Naruto Ninja Clash in the Land of Snow"} />
+        </div>
+    );
+};
+
+export default MovieCardContent;
