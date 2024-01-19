c692a7185eb4f6230ee2d61468056e2637b22623
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-19 22:27:25 +0100

    style: updated MovieCard children components and styles

--- resources/js/Components/MovieCard.tsx
+++ resources/js/Components/MovieCard.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import MovieCardImage from "./MovieCardImage";
+import MovieCardContent from "./MovieCardContent";
 
-const MovieCard = () => {
+interface MovieCardProps {
+    name: string;
+}
+
+const MovieCard = ({ name }: MovieCardProps) => {
     return (
-        <div className=" relative aspect-card ">
-            <MovieCardImage name={"1"} />
-        </div>
+        <>
+            <div className="group relative aspect-card cursor-pointer md:w-[calc((100%/6)-1rem)] transition-all duration-300  md:shrink-0 md:hover:w-[30%] md:hover:aspect-video ">
+                <MovieCardImage name={name} />
+                <MovieCardContent />
+            </div>
+        </>
     );
 };
 
