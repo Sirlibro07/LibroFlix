051dd3e94e1baa54afe05d0cc92528a67b05768b
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-21 16:47:19 +0100

    style: updated MovieCard hover styles

--- resources/js/Components/MovieCard.tsx
+++ resources/js/Components/MovieCard.tsx
@@ -9,7 +9,7 @@ interface MovieCardProps {
 const MovieCard = ({ thumbnail_path }: MovieCardProps) => {
     return (
         <>
-            <div className="group relative aspect-card cursor-pointer md:w-[calc((100%/6)-1rem)] transition-all duration-300  md:shrink-0 md:hover:w-[30%] md:hover:aspect-video ">
+            <div className="group relative aspect-card cursor-pointer transition-all duration-300 md:shrink-0 md:hover:scale-[.95]">
                 <MovieCardImage thumbnail_path={thumbnail_path} />
                 <MovieCardContent />
             </div>
