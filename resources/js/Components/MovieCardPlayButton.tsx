9248606b605a2a10bd0d2e0cd3eb09da7bc24c2e
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-21 16:13:31 +0100

    style: updated MovieCardPlayButton responsive width

--- resources/js/Components/MovieCardPlayButton.tsx
+++ resources/js/Components/MovieCardPlayButton.tsx
@@ -7,7 +7,7 @@ const MovieCardPlayButton = () => {
     };
 
     return (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[45%] md:w-[20%]">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[45%] md:w-[35%]">
             <img src={img()} alt="play-icon" decoding="async" />
         </div>
     );
