71a5f6e4fe4e0c9f891af4e193d03cba5c9c4dc0
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-21 18:50:18 +0100

    style: updated Slider html structure

--- resources/js/Components/Slider.tsx
+++ resources/js/Components/Slider.tsx
@@ -8,8 +8,10 @@ interface SliderProps {
 const Slider = ({ children }: SliderProps) => {
     return (
         <section className="relative">
-            <div className="w-full  grid grid-cols-2 gap-4 xs:grid-cols-3 md:flex md:items-center md:overflow-hidden">
-                {children}
+            <div className="overflow-hidden">
+                <div className="w-fit grid grid-cols-2 gap-4 xs:grid-cols-3 md:flex md:items-center md:overflow-hidden">
+                    {children}
+                </div>
             </div>
             <SliderButton direction="left" />
             <SliderButton direction="right" />
