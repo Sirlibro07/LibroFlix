cffcc10f8277f6aa8f2ad850fbc0571118fd202a
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-21 16:46:33 +0100

    refactor: ordered styles

--- resources/js/Components/Slider.tsx
+++ resources/js/Components/Slider.tsx
@@ -8,7 +8,7 @@ interface SliderProps {
 const Slider = ({ children }: SliderProps) => {
     return (
         <section className="relative">
-            <div className="w-full  grid grid-cols-2 gap-4  md:flex md:items-center md:overflow-hidden xs:grid-cols-3  ">
+            <div className="w-full  grid grid-cols-2 gap-4 xs:grid-cols-3 md:flex md:items-center md:overflow-hidden">
                 {children}
             </div>
             <SliderButton direction="right" />
