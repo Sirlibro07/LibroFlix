7458d074270e8ad71faee160fc506b78defc8b03
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-23 00:58:18 +0100

    style: added MovieCardTitle responsive visibility

--- resources/js/Components/MovieCardTitle.tsx
+++ resources/js/Components/MovieCardTitle.tsx
@@ -12,7 +12,7 @@ const MovieCardTitle = ({ title }: MovieCardTitleProps) => {
                 WebkitBoxOrient: "vertical",
                 WebkitLineClamp: "2",
             }}
-            className="absolute bottom-3 font-medium left-3 right-3 whitespace-pre-line overflow-hidden"
+            className="opacity-0 absolute bottom-3 font-medium left-3 right-3 whitespace-pre-line overflow-hidden md:opacity-100"
         >
             {title}
         </p>
