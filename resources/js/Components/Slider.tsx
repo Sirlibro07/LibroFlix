652ebec1ca0dd2a32186b9d13d13349a9c5890df
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-22 19:33:31 +0100

    style: added slider movement on button click

--- resources/js/Components/Slider.tsx
+++ resources/js/Components/Slider.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import SliderButton from "./SliderButton";
 
 interface SliderProps {
@@ -6,16 +6,33 @@ interface SliderProps {
 }
 
 const Slider = ({ children }: SliderProps) => {
+    const [translateX, setTranslateX] = useState(0);
+
+    const onCLickHandler = (margin: number) => {
+        setTranslateX((prevState) => prevState + margin);
+    };
+
     return (
-        <section className="relative">
-            <div className="overflow-hidden">
-                <div className="w-fit grid grid-cols-2 gap-4 xs:grid-cols-3 md:flex md:items-center md:overflow-hidden">
-                    {children}
+        <>
+            <section className="relative">
+                <div className="overflow-hidden">
+                    <div
+                        style={{ transform: `translateX(${translateX}%)` }}
+                        className={`w-fit transition-all duration-300 grid grid-cols-2 gap-4 xs:grid-cols-3 md:flex md:items-center `}
+                    >
+                        {children}
+                    </div>
                 </div>
-            </div>
-            <SliderButton direction="left" />
-            <SliderButton direction="right" />
-        </section>
+                <SliderButton
+                    direction="left"
+                    onCLickHandler={onCLickHandler}
+                />
+                <SliderButton
+                    direction="right"
+                    onCLickHandler={onCLickHandler}
+                />
+            </section>
+        </>
     );
 };
 
