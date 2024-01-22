231b1032de6b44bae2204999f7ef20696bbb9284
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-22 19:53:35 +0100

    style: added right MovieCard html

--- resources/js/Components/MovieCard.tsx
+++ resources/js/Components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MovieCardImage from "./MovieCardImage";
 import MovieCardContent from "./MovieCardContent";
+import { Link } from "@inertiajs/react";
 
 interface MovieCardProps {
     thumbnail_path: string;
@@ -13,14 +14,15 @@ const MovieCard = ({ thumbnail_path }: MovieCardProps) => {
 
     return (
         <>
-            <div
-                className={`group relative cursor-pointer aspect-card transition-all duration-300 md:shrink-0 md:${width(
+            <Link
+                className={`group relative cursor-pointer aspect-card transition-all duration-300 md:shrink-0  md:${width(
                     3
                 )} md:hover:scale-[.95] xl:${width(5)}`}
+                href={""}
             >
                 <MovieCardImage thumbnail_path={thumbnail_path} />
                 <MovieCardContent />
-            </div>
+            </Link>
         </>
     );
 };
