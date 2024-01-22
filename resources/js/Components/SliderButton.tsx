f36afea1fe3d581c448835bd4c6ffd3314e90af5
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-22 19:11:37 +0100

    style: updated SliderButton width

--- resources/js/Components/SliderButton.tsx
+++ resources/js/Components/SliderButton.tsx
@@ -4,12 +4,16 @@ import "../Helpers/CSS/slider_button.css";
 
 interface SliderButtonProps {
     direction: string;
+    onCLickHandler: (margin: number) => void;
 }
 
-const SliderButton = ({ direction }: SliderButtonProps) => {
+const SliderButton = ({ direction, onCLickHandler }: SliderButtonProps) => {
     return (
         <div
-            className={`hidden absolute top-1/2  transform -translate-y-1/2  w-[66px] h-[66px] cursor-pointer  bg-white button-${direction}  rounded-full flex justify-center items-center md:flex`}
+            onClick={() => {
+                onCLickHandler(direction === "right" ? -100 : +100);
+            }}
+            className={`hidden  absolute top-1/2  transform -translate-y-1/2  w-[66px] h-[66px] cursor-pointer  bg-white button-${direction}  rounded-full flex justify-center items-center md:flex`}
         >
             <ArrowIcon direction={direction} />
         </div>
