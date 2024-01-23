116a34e017b32ba6123f704d236283121946b117
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-23 01:08:13 +0100

    style: added MovieCards min-width

--- resources/js/Components/MovieCard.tsx
+++ resources/js/Components/MovieCard.tsx
@@ -5,19 +5,23 @@ import { Link } from "@inertiajs/react";
 
 interface MovieCardProps {
     thumbnail_path: string;
+    cards_number: number;
 }
 
-const MovieCard = ({ thumbnail_path }: MovieCardProps) => {
+const MovieCard = ({ thumbnail_path, cards_number }: MovieCardProps) => {
     const width = (number: number) => {
         return `w-[calc((100%/${number})-1rem)]`;
     };
 
+    const card_min_width = cards_number <= 5 ? "md:min-w-[250px]" : "";
+
     return (
         <>
             <Link
-                className={`group relative cursor-pointer aspect-card transition-all duration-300 md:shrink-0  md:${width(
-                    3
-                )} md:hover:scale-[.95] xl:${width(5)}`}
+                className={`group relative cursor-pointer aspect-card transition-all duration-300 
+                md:shrink-0  md:${width(3)}  md:hover:scale-[.95] xl:${width(
+                    5
+                )} ${card_min_width}`}
                 href={""}
             >
                 <MovieCardImage thumbnail_path={thumbnail_path} />
