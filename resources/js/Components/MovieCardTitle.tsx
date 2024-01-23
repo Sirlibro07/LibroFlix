a679d9f8424f93148f1b57e07d5e66aec602be7b
Libro-goloso-07 <giovanni.carninati@gmail.com>
2024-01-23 18:34:08 +0100

    style: changed MovieCardTitle breakpoint from md to lg

--- resources/js/Components/MovieCardTitle.tsx
+++ resources/js/Components/MovieCardTitle.tsx
@@ -12,7 +12,7 @@ const MovieCardTitle = ({ title }: MovieCardTitleProps) => {
                 WebkitBoxOrient: "vertical",
                 WebkitLineClamp: "2",
             }}
-            className="opacity-0 absolute bottom-3 font-medium left-3 right-3 whitespace-pre-line overflow-hidden md:opacity-100"
+            className="opacity-0 absolute bottom-3 font-medium left-3 right-3 whitespace-pre-line overflow-hidden lg:opacity-100"
         >
             {title}
         </p>
