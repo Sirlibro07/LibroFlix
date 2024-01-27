764d3855ea0d35db1010be8e38ff83ee80555e1e
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-01-27 11:17:44 +0100

    feat(style): added check on sliderButton

--- resources/js/Components/SliderButton.tsx
+++ resources/js/Components/SliderButton.tsx
@@ -4,15 +4,30 @@ import "../Helpers/CSS/slider_button.css";
 
 interface SliderButtonProps {
     direction: string;
-    onCLickHandler: (margin: number) => void;
+    setTranslateX: (value: React.SetStateAction<number>) => void;
 }
 
-const SliderButton = ({ direction, onCLickHandler }: SliderButtonProps) => {
+const SliderButton = ({ direction, setTranslateX }: SliderButtonProps) => {
+    const nextTranslate = () => {
+        setTranslateX((prevState) => prevState - 100);
+    };
+
+    const previousTranslate = () => {
+        setTranslateX((prevState) => {
+            const state = prevState < 0 ? prevState + 100 : prevState;
+            console.log(state);
+
+            return state;
+        });
+    };
+
+    const executeFunction = () => {
+        direction === "right" ? nextTranslate() : previousTranslate();
+    };
+
     return (
         <div
-            onClick={() => {
-                onCLickHandler(direction === "right" ? -100 : +100);
-            }}
+            onClick={executeFunction}
             className={`hidden  absolute top-1/2  transform -translate-y-1/2  w-[4.125rem] h-[4.125rem] cursor-pointer  bg-white button-${direction}  rounded-full flex justify-center items-center lg:flex`}
         >
             <ArrowIcon direction={direction} />
