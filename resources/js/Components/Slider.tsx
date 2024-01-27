edcc0fb67cd7523e0b0d1e955b21e00e4d778acd
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-01-27 11:18:05 +0100

    refactor(style): changed props passing in Slider

--- resources/js/Components/Slider.tsx
+++ resources/js/Components/Slider.tsx
@@ -8,22 +8,18 @@ interface SliderProps {
 const Slider = ({ children }: SliderProps) => {
     const [translateX, setTranslateX] = useState(0);
 
-    const onCLickHandler = (margin: number) => {
-        setTranslateX((prevState) => prevState + margin);
-    };
-
     return (
         <section className="relative">
             <div className="overflow-hidden">
                 <div
                     style={{ transform: `translateX(${translateX}%)` }}
-                    className={`w-fulltransition-all duration-300 grid gap-4 grid-cols-[repeat(auto-fit,minmax(140px,1fr))] lg:flex lg:items-center `}
+                    className={`w-full transition-all duration-300 grid gap-4 grid-cols-[repeat(auto-fit,minmax(140px,1fr))] lg:flex lg:items-center `}
                 >
                     {children}
                 </div>
             </div>
-            <SliderButton direction="left" onCLickHandler={onCLickHandler} />
-            <SliderButton direction="right" onCLickHandler={onCLickHandler} />
+            <SliderButton direction="left" setTranslateX={setTranslateX} />
+            <SliderButton direction="right" setTranslateX={setTranslateX} />
         </section>
     );
 };
