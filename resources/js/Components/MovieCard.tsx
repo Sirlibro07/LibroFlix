543dbf38d6d61ec4a62190e1f00b26c469728db8
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-01-29 21:05:48 +0100

    style: added MovieCard prop

--- resources/js/Components/MovieCard.tsx
+++ resources/js/Components/MovieCard.tsx
@@ -5,11 +5,12 @@ import Card from "./Card";
 
 interface MovieCardProps {
     thumbnail_path: string;
+    apply_css?: boolean;
 }
 
-const MovieCard = ({ thumbnail_path }: MovieCardProps) => {
+const MovieCard = ({ thumbnail_path, apply_css = true }: MovieCardProps) => {
     return (
-        <Card route_name={"home"} cards_number={"5"}>
+        <Card route_name={"home"} cards_number={"5"} apply_css={apply_css}>
             <MovieCardImage thumbnail_path={thumbnail_path} />
             <MovieCardPlayButton />
         </Card>
