11e0dfaf941b24024a84d848c0d5e84c25025e1a
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-01-31 18:30:13 +0100

    style: added SliderButton dynamic brightness styles

--- resources/js/Components/SliderButton.tsx
+++ resources/js/Components/SliderButton.tsx
@@ -18,10 +18,10 @@ const SliderButton = ({
 }: SliderButtonProps) => {
     const current_index = parseInt(Math.abs(TranslateX).toString()[0]);
     const can_go_next =
-        (total_cards - visible_cards * current_index) / visible_cards;
+        (total_cards - visible_cards * current_index) / visible_cards >= 1;
 
     const nextTranslate = () => {
-        can_go_next >= 1 && setTranslateX((prevState) => prevState - 100);
+        can_go_next && setTranslateX((prevState) => prevState - 100);
     };
 
     const previousTranslate = () => {
@@ -32,13 +32,26 @@ const SliderButton = ({
         direction === "right" ? nextTranslate() : previousTranslate();
     };
 
+    const low_brightness_style = "brightness-[20%]";
+
+    const previous_btn_visibility = `${
+        current_index == 0 && low_brightness_style
+    }`;
+
+    const next_btn_visibility = `${!can_go_next && low_brightness_style}`;
+
     return (
         <div
             style={
                 direction === "right" ? { right: "-17px" } : { left: "-33px" }
             }
             onClick={executeFunction}
-            className={`hidden  absolute top-1/2  transform -translate-y-1/2  w-[4.125rem] h-[4.125rem] cursor-pointer  bg-white button-${direction}  rounded-full flex justify-center items-center lg:flex`}
+            className={`hidden  absolute top-1/2  transform -translate-y-1/2  w-[4.125rem] h-[4.125rem] cursor-pointer  bg-white button-${direction}  rounded-full flex justify-center items-center lg:flex
+            ${
+                direction === "right"
+                    ? next_btn_visibility
+                    : previous_btn_visibility
+            }`}
         >
             <ArrowIcon direction={direction} />
         </div>
