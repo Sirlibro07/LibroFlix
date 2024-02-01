ef4b00b616f4012712f10644c8a199cab6e80ca2
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-01 15:43:05 +0100

    refactor(style): cleaned up Home component

--- resources/js/Pages/App/Home.tsx
+++ resources/js/Pages/App/Home.tsx
@@ -1,22 +1,34 @@
 import HomeContent from "@/Components/HomeContent";
 import MoviesContext from "@/Contexts/MoviesContext";
 import AppLayout from "@/Layouts/AppLayout";
-import React from "react";
+import React, { ReactNode } from "react";
 
 const Home = ({ auth, isLoggedIn, movies }) => {
     console.log(movies);
 
+    const renderHomeContent = (children: ReactNode) => {
+        return (
+            <AppLayout
+                user={auth.user}
+                isLoggedIn={isLoggedIn}
+                bg_image={true}
+                border={false}
+            >
+                <MoviesContext.Provider value={movies}>
+                    {children}
+                </MoviesContext.Provider>
+            </AppLayout>
+        );
+    };
+
     return (
-        <AppLayout
-            user={auth.user}
-            isLoggedIn={isLoggedIn}
-            bg_image={true}
-            border={false}
-        >
-            <MoviesContext.Provider value={movies}>
-                <HomeContent />
-            </MoviesContext.Provider>
-        </AppLayout>
+        <>
+            {renderHomeContent(
+                <>
+                    <HomeContent />
+                </>
+            )}
+        </>
     );
 };
 
