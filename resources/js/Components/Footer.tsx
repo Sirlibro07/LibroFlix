d81794c11a9f1c9d022b16c13c90e87db491aeb4
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-01 14:52:18 +0100

    style: updated Footer border styles

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -3,7 +3,7 @@ import FooterSocialsSection from "./FooterSocialsSection";
 
 const Footer = () => {
     return (
-        <footer className="body-padding flex flex-col items-center gap-y-6 py-11 z-[100] mt-[200px] border border-x-0 border-border_color lg:flex-row  justify-between">
+        <footer className="body-padding flex flex-col items-center gap-y-6 py-11 z-[100] mt-[200px] border-0 border-t border-border_color lg:flex-row  justify-between">
             <FooterSocialsSection />
             <p className="text-sm">Website by Fabio</p>
         </footer>
