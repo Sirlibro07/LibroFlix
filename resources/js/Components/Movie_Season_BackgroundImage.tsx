63624553691584519fb85f925e669540109a1ddf
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-02 16:25:36 +0100

    refactor(style): changed Movie bg-image component

--- resources/js/Components/Movie_Season_BackgroundImage.tsx
+++ resources/js/Components/Movie_Season_BackgroundImage.tsx
@@ -1,5 +1,5 @@
-import imgStoragePath from "@/Helpers/imgStoragePath";
 import React from "react";
+import BackgroundImage from "./BackgroundImage";
 
 interface Movie_Season_BackgroundImageProps {
     title: string;
@@ -9,19 +9,10 @@ const Movie_Season_BackgroundImage = ({
     title,
 }: Movie_Season_BackgroundImageProps) => {
     return (
-        <picture className={`w-full aspect-card absolute top-[90px] z-[-1]`}>
-            <source
-                media="(min-width: 768px)"
-                srcSet={imgStoragePath(`backgrounds/movies/${title}`)}
-            />
-
-            <img
-                className={`w-full h-full object-cover object-bottom`}
-                alt="background-image"
-                src={imgStoragePath(`cards/movies/${title}`)}
-                decoding="async"
-            />
-        </picture>
+        <BackgroundImage
+            folder_name={`movies/${title}`}
+            picture_classes="w-full aspect-card absolute top-[90px] z-[-1]"
+        />
     );
 };
 
