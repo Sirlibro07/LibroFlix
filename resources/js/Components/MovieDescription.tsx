775db762a49ece193cd76ad90655d7d84dc4dde5
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-02 16:05:05 +0100

    refactor(style): updated MovieDescription style

--- resources/js/Components/MovieDescription.tsx
+++ resources/js/Components/MovieDescription.tsx
@@ -4,11 +4,13 @@ import "../Helpers/CSS/first_movie_description.css";
 interface MovieDescriptionProps {
     description: string;
     className?: string;
+    slice_lines?: boolean;
 }
 
 const MovieDescription = ({
     description,
     className = "",
+    slice_lines = false,
 }: MovieDescriptionProps) => {
     return (
         <>
@@ -17,7 +19,9 @@ const MovieDescription = ({
                     display: "-webkit-box",
                     WebkitBoxOrient: "vertical",
                 }}
-                className={`first_movie_description text-light_color overflow-hidden ${className} md:text-xl`}
+                className={`${
+                    slice_lines && "first_movie_description"
+                } text-light_color overflow-hidden ${className} md:text-xl`}
             >
                 {description}
             </p>
