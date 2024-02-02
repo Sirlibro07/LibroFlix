2c26bb3a19c191ef5774feb775d9053c899d1631
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-02 16:07:34 +0100

    feat(style): updated MoviePageContent

--- resources/js/Components/MoviePageContent.tsx
+++ resources/js/Components/MoviePageContent.tsx
@@ -1,12 +1,24 @@
 import MovieContext from "@/Contexts/MovieContext";
 import React, { useContext } from "react";
+import MovieTitle from "./MovieTitle";
+import MovieInfo from "./MovieInfo";
+import MovieRating from "./MovieRating";
+import PrimaryButton from "./PrimaryButton";
+import MovieDescription from "./MovieDescription";
 
 const MoviePageContent = () => {
     const movie = useContext(MovieContext).data;
 
-    console.log(movie);
+    return (
+        <div className="body-padding py-4 h-fit mt-[300px] bg-black rounded-tl-[25px] rounded-tr-[25px]">
+            <MovieTitle title={movie.title} />
+            <MovieInfo date={movie.year} />
+            <MovieRating rating={movie.rating} className={""} />
+            <PrimaryButton>Watch Now</PrimaryButton>
 
-    return <h1>ciao</h1>;
+            <MovieDescription description={movie.description} />
+        </div>
+    );
 };
 
 export default MoviePageContent;
