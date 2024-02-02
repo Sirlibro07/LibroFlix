3aa0edfae46d16261e4043cd106d52947f7d8b02
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-03 00:05:33 +0100

    style: updated NavbarAccountLinks styles

--- resources/js/Components/NavbarAccountLinks.tsx
+++ resources/js/Components/NavbarAccountLinks.tsx
@@ -4,9 +4,9 @@ import PrimaryButton from "./PrimaryButton";
 
 const NavbarAccountLinks = () => {
     return (
-        <div className="text-base w-full  sm:flex sm:gap-x-3 lg:absolute lg:top-0 lg:right-4 lg:flex lg:items-center lg:h-full lg:w-fit">
+        <div className="text-base w-full flex gap-3 px-4 lg:absolute lg:top-0 lg:right-4 lg:flex lg:items-center lg:h-full lg:w-fit">
             <a href={route("login")} className="block w-full lg:w-auto lg:px-4">
-                <SecondaryButton className="w-full mb-3 lg:px-4 lg:mb-0">
+                <SecondaryButton className="w-full lg:px-4">
                     Log In
                 </SecondaryButton>
             </a>
