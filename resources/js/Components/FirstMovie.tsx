d8a7228b8759323bdfb1a64f8ee2a7290631e756
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-02 18:28:22 +0100

    refactor(style): Updated FirstMovie data

--- resources/js/Components/FirstMovie.tsx
+++ resources/js/Components/FirstMovie.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 MovieTitle;
 import MovieInfo from "./MovieInfo";
 import MovieDescription from "./MovieDescription";
 import MovieTitle from "./MovieTitle";
 import MovieRating from "./MovieRating";
 import FirstMovieLinks from "./FirstMovieLinks";
+import MoviesContext from "@/Contexts/MoviesContext";
 
 const FirstMovie = () => {
+    const movie3 = useContext(MoviesContext).data[3];
+    console.log(movie3);
+
     return (
         <>
             <div className="text-center z-1 md:text-start mb-16 md:mb-[120px]   ">
@@ -15,21 +19,20 @@ const FirstMovie = () => {
                     className="hidden md:flex md:justify-start md:mb-[-0.25rem] md:mt-4"
                 />
                 <MovieTitle
-                    title={`Naruto Shippuden:\nThe Movie`}
+                    title={movie3.title}
                     className="text-mobile_h1 whitespace-pre-line md:text-desktop_h1 md:leading-[120%]"
                 />
-                <MovieRating rating={5} className="hidden md:flex my-2" />
+                <MovieRating
+                    rating={movie3.rating}
+                    className="hidden md:flex my-2"
+                />
                 <MovieInfo
-                    date={2007}
+                    date={movie3.year}
                     className="mt-2 justify-center md:hidden"
                 />
                 <MovieDescription
-                    className="mb-4 lg:w-4/6 xl:w-1/2 2xl:w-5/12"
-                    description="So I started to walk into the water. I won't lie to you boys, I was
-                                terrified. But I pressed on, and as I made my way past the breakers
-                                a strange calm came over me. I don't know if it was divine
-                                intervention or the kinship of all living things but I tell you
-                                Jerry at that moment, I was a marine biologist."
+                    className="mb-4 lg:w-4/6 xl:w-1/2 2xl:w-5/12 line-clamp-2 md:line-clamp-3"
+                    description={movie3.description}
                 />
 
                 <FirstMovieLinks />
