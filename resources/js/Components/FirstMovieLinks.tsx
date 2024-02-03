1d4410c18166349c3d6cbcc58b2392b84e2432ca
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-03 01:00:31 +0100

    refactor(style): cleaned up FirstMovieLinks

--- resources/js/Components/FirstMovieLinks.tsx
+++ resources/js/Components/FirstMovieLinks.tsx
@@ -1,28 +1,16 @@
 import React from "react";
-import GeneralLink from "./GeneralLink";
-import GeneralLinkWithIcon from "./GeneralLinkWithIcon";
+import ButtonWithIcon from "./ButtonWithIcon";
+import { Link } from "@inertiajs/react";
+import Button from "./Button";
 
 const FirstMovieLinks = ({ title }) => {
     return (
-        <>
-            <GeneralLink
-                route_name={"home"}
-                border={false}
-                className="bg-brand font-medium block text-center py-[0.625rem] mt-6 md:hidden "
-            >
-                Watch Now
-            </GeneralLink>
-            <div className="w-full  gap-x-5 hidden md:flex ">
-                <GeneralLinkWithIcon
-                    route_name={`movies.show`}
-                    route_parameter={title}
-                    className="bg-brand"
-                    icon="plus"
-                >
-                    Learn More
-                </GeneralLinkWithIcon>
-            </div>
-        </>
+        <Link className="" href={route("movies.show", title)}>
+            <Button className="w-full bg-brand md:hidden">Watch Now</Button>
+            <ButtonWithIcon className="hidden md:block">
+                Learn More
+            </ButtonWithIcon>
+        </Link>
     );
 };
 
