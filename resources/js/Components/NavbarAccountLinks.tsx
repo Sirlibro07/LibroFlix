fe6d930db76ba5e082a87f34d8bcd8324942266b
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-03 01:24:29 +0100

    refactor(style): switched Primary and Secondary buttons with Button

--- resources/js/Components/NavbarAccountLinks.tsx
+++ resources/js/Components/NavbarAccountLinks.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import SecondaryButton from "./SecondaryButton";
 import PrimaryButton from "./PrimaryButton";
+import Button from "./Button";
 
 const NavbarAccountLinks = () => {
     return (
         <div className="text-base w-full flex gap-3 px-4 lg:absolute lg:top-0 lg:right-4 lg:flex lg:items-center lg:h-full lg:w-fit">
             <a href={route("login")} className="block w-full lg:w-auto lg:px-4">
-                <SecondaryButton className="w-full lg:px-4">
-                    Log In
-                </SecondaryButton>
+                <Button className="w-full lg:px-4">Log in</Button>
             </a>
             <a href={route("register")} className="block w-full lg:w-auto">
-                <PrimaryButton className="w-full lg:px-4">
-                    Sign Up
-                </PrimaryButton>
+                <Button className="w-full bg-brand lg:px-4 ">Sign up</Button>
             </a>
         </div>
     );
