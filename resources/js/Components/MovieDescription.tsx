a0857ba09bc7f43d123d824710b4598030b52b6b
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-03 02:11:36 +0100

    refactor(style): deleted unused css

--- resources/js/Components/MovieDescription.tsx
+++ resources/js/Components/MovieDescription.tsx
@@ -4,14 +4,12 @@ import "../Helpers/CSS/first_movie_description.css";
 interface MovieDescriptionProps {
     description: string;
     className?: string;
-    slice_lines?: boolean;
     description_p?: boolean;
 }
 
 const MovieDescription = ({
     description,
     className = "",
-    slice_lines = false,
     description_p = true,
 }: MovieDescriptionProps) => {
     return (
@@ -19,11 +17,7 @@ const MovieDescription = ({
             {description_p && (
                 <p className="mt-10 mb-4 font-medium">Description:</p>
             )}
-            <p
-                className={`${
-                    slice_lines && "first_movie_description"
-                } text-light_color overflow-hidden ${className}`}
-            >
+            <p className={` text-light_color overflow-hidden ${className}`}>
                 {description}
             </p>
         </>
