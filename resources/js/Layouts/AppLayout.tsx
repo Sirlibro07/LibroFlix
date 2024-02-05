da266c0417cce1c1b6d9277954c763b8d8d0d1f4
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-05 23:37:34 +0100

    refactor(style): @/Helpers/CSS -> @/CSS

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -1,9 +1,9 @@
 import React, { ReactNode, useState } from "react";
-import "@/Helpers/CSS/borders.css";
-import "@/Helpers/CSS/navbar_visibility.css";
-import "@/Helpers/CSS/AppLayout.css";
-import "@/Helpers/CSS/letter_spacing.css";
-import "@/Helpers/CSS/scrollbar.css";
+import "@/CSS/borders.css";
+import "@/CSS/navbar_visibility.css";
+import "@/CSS/AppLayout.css";
+import "@/CSS/letter_spacing.css";
+import "@/CSS/scrollbar.css";
 import Menu_Searchbar_Context from "@/Contexts/Menu_Searchbar_Context";
 import UserContext from "@/Contexts/UserContext";
 import Navbar from "@/Components/Navbar";
