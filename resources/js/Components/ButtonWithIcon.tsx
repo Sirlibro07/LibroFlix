607cc90fdde74c31ffaa8c7858d9392d31711cbe
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-06 03:18:29 +0100

    style: added ButtonWithIcon prop

--- resources/js/Components/ButtonWithIcon.tsx
+++ resources/js/Components/ButtonWithIcon.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import Button from "./Button";
 import Icon from "./Icon";
 
-const ButtonWithIcon = ({ className = "", children, icon }) => {
+const ButtonWithIcon = ({
+    className = "",
+    children,
+    icon = "",
+    icon_full_classname = "",
+}) => {
     return (
         <Button className={`bg-brand pl-3 pr-4 ${className}`}>
-            <Icon icon={icon} className="mr-3  text-[1.125rem]" />
+            <Icon
+                icon={icon}
+                icon_full_classname={icon_full_classname}
+                className="mr-3  text-[1.125rem]"
+            />
             {children}
         </Button>
     );
