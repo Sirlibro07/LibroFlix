d0a35843702f220904e3beeeebdfb32375035aa8
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-06 15:14:20 +0100

    fix(style): prevent input icon from intercepting input click

--- resources/js/Components/FormInputIcon.tsx
+++ resources/js/Components/FormInputIcon.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import Icon from "./Icon";
 
 const FormInputIcon = ({ icon }) => {
-    return <Icon icon={icon} className="absolute left-4 text-xl opacity-50" />;
+    return (
+        <Icon
+            icon={icon}
+            className="absolute left-4 pointer-events-none text-xl opacity-50"
+        />
+    );
 };
 
 export default FormInputIcon;
