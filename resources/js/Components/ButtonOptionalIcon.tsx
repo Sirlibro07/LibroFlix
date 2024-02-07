49f1942221f2842cefba4b558510cc2e70b31c2d
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-07 13:27:07 +0100

    fix(style): added ButtonOptionalIcon Icon breakpoint styles

--- resources/js/Components/ButtonOptionalIcon.tsx
+++ resources/js/Components/ButtonOptionalIcon.tsx
@@ -3,12 +3,10 @@ import ButtonWithIcon from "./ButtonWithIcon";
 import IconType from "@/Shared/interfaces/IconType";
 
 interface ButtonOptionalIconProps extends IconType {
-    icon_className?: string;
     children: ReactNode;
 }
 
 const ButtonOptionalIcon = ({
-    icon_className = "",
     icon_type,
     icon_name,
     children,
@@ -18,7 +16,7 @@ const ButtonOptionalIcon = ({
             icon_type={icon_name}
             icon_name={icon_type}
             className="w-full pl-0 pr-0 bg_brand md:block md:pl-3 md:pr-4 md:w-fit"
-            icon_className={icon_className}
+            icon_className="hidden md:inline"
         >
             {children}
         </ButtonWithIcon>
