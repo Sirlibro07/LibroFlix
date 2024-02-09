d1f85d0cd2b3212c9b9d7c3f9eed64faceadaff7
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-09 16:53:40 +0100

    chore: added routes comments

--- routes/auth.php
+++ routes/auth.php
@@ -12,22 +12,26 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    // Register
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
+    // Login
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
+    // Reset Password Email
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
         ->name('password.email');
 
+    // Reset Password
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
         ->name('password.reset');
 
@@ -35,20 +39,23 @@ Route::middleware('guest')->group(function () {
         ->name('password.store');
 });
 
-
+// Email Verification
 Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
 
 Route::middleware('auth')->group(function () {
 
+    // Email Verification Notification
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
 
+    // Update password 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
+    // Logout
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
