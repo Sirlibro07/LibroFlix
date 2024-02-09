d50617b89416c646276d8ffa5e44c4e3191e2b75
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-09 16:53:57 +0100

    style: updated MovieBlur styles

--- resources/js/Components/MovieBlur.tsx
+++ resources/js/Components/MovieBlur.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MovieBlur = () => {
     return (
-        <div className="hidden w-calc(100%+100px) ml-[-3.125rem] h-[3.125rem] bg-black blur-lg  mt-[25rem] md:block"></div>
+        <div className="hidden w-calc(100%+100px) ml-[-3.125rem] h-[3.125rem] bg-black blur-[8px]  mt-[25rem] md:block"></div>
     );
 };
 
