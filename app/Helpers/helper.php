20544f7eadcb370a2aadb2a7026d52615e5061b4
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-09 18:49:00 +0100

    refactor: update helper.php

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\URL;
 
 function temporaryEmailVerificationSignedRoute($user): string
 {
-    return SignedRoute("verification.verify", 30, $user);
+    return SignedRoute("verification.verify", 30, $user, $user->email_verification_token);
 }
 
 
-function SignedRoute(string $route, int $minutes, object $user): string
+function SignedRoute(string $route, int $minutes, object $user, string $hash): string
 {
-    return URL::temporarySignedRoute($route, now()->addMinutes($minutes), ['id' => $user->id, 'hash' => $user->email_verification_token]);
+    return URL::temporarySignedRoute($route, now()->addMinutes($minutes), ['id' => $user->id, 'hash' => $hash]);
 }
