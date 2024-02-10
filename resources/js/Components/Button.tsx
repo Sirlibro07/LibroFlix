8f7d3f45b912930b777326510eb7437976d56f2f
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-10 16:07:37 +0100

    update(style): updated Button component

--- resources/js/Components/Button.tsx
+++ resources/js/Components/Button.tsx
@@ -4,11 +4,21 @@ interface ButtonProps {
     className?: string;
     disabled?: boolean;
     children: ReactNode;
+    onClick?: () => void;
+    type?: string;
 }
 
-const Button = ({ className = "", disabled = null, children }: ButtonProps) => {
+const Button = ({
+    className = "",
+    disabled = null,
+    children,
+    onClick = null,
+    type = "",
+}: ButtonProps) => {
     return (
         <button
+            type={type}
+            onClick={onClick && onClick}
             disabled={disabled}
             className={`h-elements_height text-base font-medium rounded-10 ${className} ${
                 disabled && "opacity-90"
