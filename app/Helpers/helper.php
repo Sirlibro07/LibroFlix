7d6c19b64698682e10dbe718f2c81fdb7e956893
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-10 16:00:19 +0100

    update: deleted email verification route middleware

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 
 
@@ -12,4 +15,15 @@ function temporaryEmailVerificationSignedRoute($user): string
 function SignedRoute(string $route, int $minutes, object $user, string $hash): string
 {
     return URL::temporarySignedRoute($route, now()->addMinutes($minutes), ['id' => $user->id, 'hash' => $hash]);
+}
+
+
+function sendEmail(Mailable $mail)
+{
+    try {
+        Log::info("tried to send email");
+        Mail::send($mail);
+    } catch (Exception $e) {
+        Log::error("something went wrong ! " . $e->getMessage());
+    }
 }
--- routes/auth.php
+++ routes/auth.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetEmailController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -47,9 +48,7 @@ Route::middleware('auth')->group(function () {
 
     // Email Verification Notification
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
         ->name('verification.send');
-
 
     // Update password 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
