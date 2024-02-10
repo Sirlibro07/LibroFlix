5536730579c069051de3eb35e369600d250016cc
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-10 16:01:38 +0100

    refactor: corrected EmailVerificationNotificationController logic

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SendEmailJob;
 use App\Mail\VerificationEmail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Mail;
 
 class EmailVerificationNotificationController extends Controller
 {
-    /**
-     * Send a new email verification notification.
-     */
     public function store(Request $request)
     {
         $user = $request->user();
@@ -20,7 +16,9 @@ class EmailVerificationNotificationController extends Controller
 
             $url = temporaryEmailVerificationSignedRoute($request->user());
 
-            Mail::send(new VerificationEmail($url));
+            SendEmailJob::dispatch(new VerificationEmail($user, $url));
+
+            return back()->with("status", "email sent");
         }
     }
 }
