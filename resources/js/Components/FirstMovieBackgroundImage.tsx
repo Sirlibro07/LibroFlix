b533b785dd0fca150299442e0302967ac6d24d35
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-11 03:32:48 +0100

    update(style): added FirstMovieBackgroundImage opacity

--- resources/js/Components/FirstMovieBackgroundImage.tsx
+++ resources/js/Components/FirstMovieBackgroundImage.tsx
@@ -3,7 +3,7 @@ import BackgroundImage from "./BackgroundImage";
 
 const FirstMovieBackgroundImage = () => {
     return (
-        <div className="w-full h-[44rem] md:h-[48.75rem] absolute top-navbar_height bottom-0 z-[-1]">
+        <div className="w-full h-[44rem] md:h-[48.75rem] opacity-[.85] absolute top-navbar_height bottom-0 z-[-1]">
             <BackgroundImage
                 folder_name="first_movie"
                 picture_classes="w-full h-full"
