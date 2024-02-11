482a98d4dbd501913f83039cf39b2c16bb060d4a
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-11 03:36:33 +0100

    refactor(style): deleted useless FirstMovieBackgroundImage component

--- resources/js/Pages/App/Home.tsx
+++ resources/js/Pages/App/Home.tsx
@@ -1,4 +1,4 @@
-import FirstMovieBackgroundImage from "@/Components/FirstMovieBackgroundImage";
+import BackgroundImage from "@/Components/BackgroundImage";
 import HomeContent from "@/Components/HomeContent";
 import MoviesContext from "@/Contexts/MoviesContext";
 import AppLayout from "@/Layouts/AppLayout";
@@ -9,7 +9,15 @@ const Home = ({ movies }) => {
 
     const renderHomeContent = (children: ReactNode) => {
         return (
-            <AppLayout bg_image={<FirstMovieBackgroundImage />} border={false}>
+            <AppLayout
+                bg_image={
+                    <BackgroundImage
+                        folder_name="first_movie"
+                        picture_classes="w-full h-[44rem] opacity-[.85] z-[-1] top-navbar_height md:h-[48.75rem]"
+                    />
+                }
+                border={false}
+            >
                 <MoviesContext.Provider value={movies}>
                     {children}
                 </MoviesContext.Provider>
