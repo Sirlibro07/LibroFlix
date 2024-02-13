9a3a089fb130a6d4477b94fa2c1e59688a5c3cf4
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-13 02:56:43 +0100

    update(style): updated footer breakpoint

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = ({ className = "", border = true }) => {
         <footer
             className={`body-padding absolute bottom-0 left-0 w-full flex flex-col items-center gap-y-6 py-11  ${
                 border && "border-0 border-t border-grey"
-            } lg:flex-row  justify-between ${className}`}
+            } md:flex-row  justify-between ${className}`}
         >
             <FooterSocialsSection />
             <p className="text-sm">Website by Fabio</p>
