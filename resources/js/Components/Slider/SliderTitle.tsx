3f43172a2ead6fbe76f981f95ba3632711273747
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-14 12:25:11 +0100

    fix(style): added right SliderTitle props

--- resources/js/Components/Slider/SliderTitle.tsx
+++ resources/js/Components/Slider/SliderTitle.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export interface SliderTitleProps {
     title: string;
-    slider_title_className: string;
+    slider_title_className?: string;
 }
 
-const SliderTitle = ({ title, slider_title_className }: SliderTitleProps) => {
+const SliderTitle = ({
+    title,
+    slider_title_className = "",
+}: SliderTitleProps) => {
     return (
         <p
             className={`text-typescale_23_px font-medium mb-6 md:text-typescale_33_px ${slider_title_className}`}
