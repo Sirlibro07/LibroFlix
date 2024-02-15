0f81ad2e17e1eb788b42e55e4de6e0b03016e42d
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-15 14:19:01 +0100

    fix(auth): added correct exception in LoginUser listener

--- app/Listeners/LoginUser.php
+++ app/Listeners/LoginUser.php
@@ -3,14 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\LoginRequested;
-use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Session;
 
 class LoginUser
 {
@@ -29,10 +25,10 @@ class LoginUser
     {
         try {
             if (!Auth::attempt(['email' => $event->email, 'password' => $event->password])) {
-                throw new Exception();
+                throw new AuthenticationException();
             }
-            session()->regenerate();
 
+            session()->regenerate();
             return redirect(RouteServiceProvider::HOME);
         } catch (AuthenticationException $e) {
             return redirect()->back()->withErrors(['email' => 'Credentials not valid']);
