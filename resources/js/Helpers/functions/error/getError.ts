94d274bc64eed792ec7ee303a4f4b27d0eb3b908
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-26 19:07:27 +0100

    refacxtor(style): used built-in method instead of for-loop in getError

--- resources/js/Helpers/functions/error/getError.ts
+++ resources/js/Helpers/functions/error/getError.ts
@@ -1,12 +1,10 @@
 import { ErrorInfoType } from "@/Pages/Error/ErrorPage";
 
 const getError = (http_status_code: number, errors: ErrorInfoType[]) => {
-    for (let i = 0; i < errors.length; i++) {
-        if (http_status_code === errors[i].http_status_code) {
-            return errors[i];
-        }
-    }
-    return errors[errors.length - 1];
+    const error_found = errors.find(
+        (error) => error.http_status_code === http_status_code
+    );
+    return error_found ? error_found : errors[errors.length - 1];
 };
 
 export default getError;
