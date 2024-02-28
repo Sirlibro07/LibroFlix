adde890684e44e8af9911d3a6faf081e978a711b
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-02-28 02:31:58 +0100

    feat(email): added abort on email sending

--- app/Services/SendEmailService.php
+++ app/Services/SendEmailService.php
@@ -9,6 +9,7 @@ class SendEmailService
 {
     public function sendEmailAsJob(Mailable $mail): void
     {
+        abort(500); // add to do this bc I don't have an actual email infrastructure/provider/plan
         SendEmailJob::dispatch($mail);
     }
 }
