18e942e006253a0e148b9a7a945fb31f5448ee62
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-03-02 17:11:28 +0100

    fix(migration): fixed watchlist_items migration

--- database/migrations/2024_02_17_165855_create_watchlist_items_table.php
+++ database/migrations/2024_02_17_165855_create_watchlist_items_table.php
@@ -12,10 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('watchlist_items', function (Blueprint $table) {
-            $table->id();
             $table->foreignId("user_id")->constrained("users");
             $table->foreignId("movie_id")->constrained("movies");
-            $table->timestamps();
         });
     }
 
