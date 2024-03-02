09b5ac2d770d507c6f921d301750758f347d48cd
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-03-02 01:04:43 +0100

    chore: deleted unused namespaces

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -4,8 +4,6 @@ namespace App\Services;
 
 use App\Http\Resources\MovieResource;
 use App\Models\Movie;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MovieService
