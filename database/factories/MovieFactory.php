03858468496ec228d0ee7a8d896835638c5bc0e5
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-03-02 17:40:21 +0100

    test: added MovieFactory

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'description' => fake()->paragraph(),
+            'rating' => fake()->randomFloat(1, 0, 5),
+            'year' => fake()->year(),
+        ];
+    }
+}
