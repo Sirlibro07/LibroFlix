f6f85ecb343942a44a305e728dfde66bcdff5921
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-03-05 19:00:11 +0100

    chore(email): updated comment

--- app/Services/SendEmailService.php
+++ app/Services/SendEmailService.php
@@ -10,7 +10,7 @@ class SendEmailService
     public function sendEmailAsJob(Mailable $mail): void
     {
         if (!app()->environment(['local', 'testing'])) {
-            abort(500); // add to do this bc I don't have an actual email infrastructure/provider/plan
+            abort(500); // had to do this bc I don't have an actual email infrastructure/provider/plan
         }
         SendEmailJob::dispatch($mail);
     }
