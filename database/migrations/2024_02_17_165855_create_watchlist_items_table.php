ad199f8a887c40260cc0c9f7f8411ec38aab7246
Sirlibro07 <152784690+Sirlibro07@users.noreply.github.com>
2024-03-13 12:51:06 +0100

    refactor(migration): deleted useless table specification

--- database/migrations/2024_02_17_165855_create_watchlist_items_table.php
+++ database/migrations/2024_02_17_165855_create_watchlist_items_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('watchlist_items', function (Blueprint $table) {
-            $table->foreignId("user_id")->constrained("users");
-            $table->foreignId("movie_id")->constrained("movies");
+            $table->foreignId("user_id")->constrained();
+            $table->foreignId("movie_id")->constrained();
         });
     }
 
